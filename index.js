2840bcb23e017fc1e75739966136561cbd0b355e
Andrew Haddock <ahaddock523@gmail.com>
2020-02-13 18:39:13 -0700

    stood up express web-server and created initial get and post calls for login. Included body-parser middleware to parse incoming requests.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello there');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="password confirmation" />
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `);
+});
+
+app.post('/', bodyParser.urlencoded({ extended: true }), (req, res) => {
+	console.log(req.body);
+	res.send('Accound created!!!');
+});
+
+app.listen(3000, () => {
+	console.log('listening');
+});
